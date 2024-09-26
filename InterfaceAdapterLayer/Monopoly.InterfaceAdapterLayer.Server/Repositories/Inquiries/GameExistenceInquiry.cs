61edbe0779e77f9e6912141bd06638b3e860d102
aa89227 <aa89227@gmail.com>
2024-09-26 21:20:14 +0800

    refactor: 將 IGameExistenceInquiry 中的方法改為非同步並更新對應實作

--- InterfaceAdapterLayer/Monopoly.InterfaceAdapterLayer.Server/Repositories/Inquiries/GameExistenceInquiry.cs
+++ InterfaceAdapterLayer/Monopoly.InterfaceAdapterLayer.Server/Repositories/Inquiries/GameExistenceInquiry.cs
@@ -6,9 +6,9 @@ namespace Monopoly.InterfaceAdapterLayer.Server.Repositories.Inquiries;
 /// <inheritdoc />
 internal class GameExistenceInquiry(FakeInMemoryDatabase<MonopolyAggregate> database) : IGameExistenceInquiry
 {
-    public bool CheckGameExistence(string gameId)
+    public async Task<bool> CheckGameExistenceAsync(string gameId)
     {
-        var monopoly = database.FindByIdAsync(gameId);
+        var monopoly = await database.FindByIdAsync(gameId);
         return monopoly is not null;
     }
 }
